5bf6146139634bccf6817f6d7dcaa6709c2b11fa
jmunozsuastegui <jmunozsuastegui@ggc.edu>
2023-01-26 08:30:13 -0500

    added commented line.

---
+++ src/Chocalate.java
@@ -0,0 +1,18 @@
+/**
+ * /**
+ *  * Class Name: Chocolate
+ *  * @Author: Jose Munoz Suastegui
+ *  * @Version: 1.0
+ *  * @Date: January 2023
+ *  *
+ *  * Description: A program designed to calculate the speed of light
+ *  */
+ */
+
+
+
+
+
+public class Chocolate {
+
+}
---
+++ src/SpeedLight.java
@@ -0,0 +1,23 @@
+/**
+ * Class Name: SpeedLight
+ * @Author: Jose Munoz Suastegui
+ * @Version: 1.0
+ * @Date: January 2023
+ * Description: A program designed to calculate the speed of light
+ */
+
+
+
+
+
+
+
+
+
+
+public class SpeedLight {
+    public static void main(String []args){
+        //edited
+
+    }
+}
