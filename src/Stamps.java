2ee8683b9375e2c32f48af3f02b407fe716e767e
jmunozsuastegui <jmunozsuastegui@ggc.edu>
2023-02-02 01:23:37 -0500

    updated

---
+++ src/Stamps.java
@@ -0,0 +1,16 @@
+public class Stamps {
+    public static void main(String [] args){
+         int susan = 8;
+         int jean = 40;
+
+         int difference = jean - susan;
+
+
+         System.out.println("Susan and Jean just started collecting stamps as a hobby. Susan currently has "
+                 + susan + " stamps, while Jean has only " + jean + " stamps.");
+
+         System.out.println("How many more does Jean have than Susan?");
+
+         System.out.println("The answer to this will be: " + difference);
+    }
+}
