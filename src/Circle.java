454400f31aaea9576c92c86cfa219f655eee3188
jmunozsuastegui <jmunozsuastegui@ggc.edu>
2023-02-02 20:56:50 -0500

    updated

---
+++ src/Circle.java
@@ -0,0 +1,10 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 9.5;
+        double area = radius * radius * Math.PI;
+        double perimeter = 2 * radius * Math.PI;
+
+        System.out.println("Area of the circle: " + area);
+        System.out.println("Perimeter of the circle: " + perimeter);
+    }
+}
